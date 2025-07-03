071e2cf0f172f55ae744e1c46c91a5664818fce5
GUSTAVO FERNANDES TOLEDO <gustavo.toledo@cs.up.edu.br>
2025-07-03 09:28:05 -0300

    atualização banco

---
+++ BIBLIOTECA/BIBLIOTECA/API/Program.cs
@@ -0,0 +1,19 @@
+using API.Modelos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<BibliotecaDbContext>();
+var app = builder.Build();
+
+
+
+
+
+
+
+
+
+
+app.Run();
